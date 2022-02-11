11dc20717d8651ff335d143a09f07fd87db35615
MaxKoval1ov <maxkov112211@gmail.com>
2022-02-11 20:20:44 +0300

    feat: add ts-dependencies

--- src/common/config.js
+++ src/common/config.js
@@ -5,10 +5,18 @@ dotenv.config({
   path: path.join(__dirname, '../../.env')
 });
 
-module.exports = {
-  PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV,
-  MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-  AUTH_MODE: process.env.AUTH_MODE === 'true'
+  const PORT = process.env.PORT || 4000;
+  const {NODE_ENV} = process.env;
+  const {MONGO_CONNECTION_STRING} = process.env;
+  const {JWT_SECRET_KEY} = process.env;
+  const AUTH_MODE = process.env.AUTH_MODE === 'true';
+  const LEVEL_LOG = process.env.LEVEL_LOG || '4';
+
+export {
+  PORT,
+  NODE_ENV,
+  MONGO_CONNECTION_STRING,
+  JWT_SECRET_KEY,
+  AUTH_MODE,
+  LEVEL_LOG,
 };
